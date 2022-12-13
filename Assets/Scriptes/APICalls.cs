103885e6db8ad878035166c70df6d78f3198398e
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-12-13 14:44:20 -0600

    Fix 2

--- Assets/Scriptes/APICalls.cs
+++ Assets/Scriptes/APICalls.cs
@@ -15,38 +15,12 @@ public class APICalls : MonoBehaviour
     public TMP_InputField input;
 
     public void testCall(){
-        StartCoroutine(EnuJira(input.text ,returnValue => 
+        StartCoroutine(JiraCall.EnuJira(input.text ,returnValue => 
         {
             textbox.text = returnValue;
         }
         ));
 
-        
-       //textbox.text = StartCoroutine(EnuJira());
     }
-
-
-    IEnumerator EnuJira(string token, System.Action<string> callback = null){
-        string email = "manuel.paurevic@oasisdigital.com";
-        string authCache = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(email + ":" + token));
-
-        UnityWebRequest request = UnityWebRequest.Get("https://oasisintern.atlassian.net/rest/api/latest/myself");
-        request.SetRequestHeader("Authorization", "Basic " + authCache);
-
-        yield return request.SendWebRequest();
-
-        if(request.isNetworkError || request.isHttpError){
-            Debug.Log("Error while Receiving: " + request.error);
-            yield return request.error;
-            callback.Invoke(request.error);
-        }else{
-            string result = request.downloadHandler.text;
-            callback.Invoke(result);
-        }
-    }
-
-
-
-
 
 }
--- Assets/Scriptes/JiraCall.cs
+++ Assets/Scriptes/JiraCall.cs
@@ -5,22 +5,24 @@ using System.Text;
 using UnityEngine;
 using System.Collections;
 
-public class JiraCall
+public static class JiraCall
 {
-    public IEnumerator EnuJira(string token){
+    public static IEnumerator EnuJira(string token, System.Action<string> callback = null){
         string email = "manuel.paurevic@oasisdigital.com";
         string authCache = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(email + ":" + token));
 
         UnityWebRequest request = UnityWebRequest.Get("https://oasisintern.atlassian.net/rest/api/latest/myself");
         request.SetRequestHeader("Authorization", "Basic " + authCache);
-        
+
         yield return request.SendWebRequest();
 
         if(request.isNetworkError || request.isHttpError){
             Debug.Log("Error while Receiving: " + request.error);
+            yield return request.error;
+            callback.Invoke(request.error);
         }else{
             string result = request.downloadHandler.text;
-            Debug.Log(result);
+            callback.Invoke(result);
         }
     }
 
