b2f4656f2dc09174a32830c99db6ce736d15b8c2
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-12-15 13:31:17 -0600

    Added Origin Control

--- Assets/Scriptes/APICalls.cs
+++ Assets/Scriptes/APICalls.cs
@@ -34,11 +34,36 @@ public class APICalls : MonoBehaviour
         //string token = input.text;
         //textbox.text = jsAPICall(token);
         
+    }
 
+    public void testCallB(){
+        
+        StartCoroutine(JiraCall.EnuJiraB(input.text ,returnValue => 
+        {
+            user1 = JsonUtility.FromJson<User>(returnValue);
+            //textbox.text = returnValue;
+            textbox.text = user1.emailAddress;
+        }
+        ));
+    
+        //string token = input.text;
+        //textbox.text = jsAPICall(token);
+        
+    }
 
-
-
-
+    public void testCallC(){
+        
+        StartCoroutine(JiraCall.EnuJiraC(input.text ,returnValue => 
+        {
+            user1 = JsonUtility.FromJson<User>(returnValue);
+            //textbox.text = returnValue;
+            textbox.text = user1.emailAddress;
+        }
+        ));
+    
+        //string token = input.text;
+        //textbox.text = jsAPICall(token);
+        
     }
 
 }
--- Assets/Scriptes/JiraCall.cs
+++ Assets/Scriptes/JiraCall.cs
@@ -13,7 +13,52 @@ public static class JiraCall
 
         UnityWebRequest request = UnityWebRequest.Get("https://oasisintern.atlassian.net/rest/api/latest/myself");
         request.SetRequestHeader("Authorization", "Basic " + authCache);
-        //request.SetRequestHeader("Authorization", "Basic " + authCache);
+        request.SetRequestHeader("Access-Control-Allow-Origin", "*");
+        
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.ProtocolError){
+            Debug.Log("Error while Receiving: " + request.error);
+            string result = request.downloadHandler.text;
+            callback.Invoke(result);
+            //yield return request.error;
+            //callback.Invoke(request.error);
+        }else{
+            string result = request.downloadHandler.text;
+            callback.Invoke(result);
+        }
+    }
+
+
+    public static IEnumerator EnuJiraB(string token, System.Action<string> callback = null){
+        string email = "manuel.paurevic@oasisdigital.com";
+        string authCache = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(email + ":" + token));
+
+        UnityWebRequest request = UnityWebRequest.Get("https://oasisintern.atlassian.net/rest/api/latest/myself");
+        request.SetRequestHeader("Authorization", "Basic " + authCache);
+        request.SetRequestHeader("Access-Control-Allow-Origin", "https://manuelpaurevic.github.io/WebJira/");
+        
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.ProtocolError){
+            Debug.Log("Error while Receiving: " + request.error);
+            string result = request.downloadHandler.text;
+            callback.Invoke(result);
+            //yield return request.error;
+            //callback.Invoke(request.error);
+        }else{
+            string result = request.downloadHandler.text;
+            callback.Invoke(result);
+        }
+    }
+
+    public static IEnumerator EnuJiraC(string token, System.Action<string> callback = null){
+        string email = "manuel.paurevic@oasisdigital.com";
+        string authCache = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(email + ":" + token));
+
+        UnityWebRequest request = UnityWebRequest.Get("https://oasisintern.atlassian.net/rest/api/latest/myself");
+        request.SetRequestHeader("Authorization", "Basic " + authCache);
+        request.SetRequestHeader("Access-Control-Allow-Origin", "https://oasisintern.atlassian.net/rest/api/latest/myself");
         
         yield return request.SendWebRequest();
 
